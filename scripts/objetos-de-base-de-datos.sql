d39cd4ab5cc5a2267d417fb0bf1f8c015dde5e21
Christian <adrianAndradeChristian@gmail.com>
2026-01-06 14:47:23 -0500

    Add images table to database schema

--- scripts/objetos-de-base-de-datos.sql
+++ scripts/objetos-de-base-de-datos.sql
@@ -5,3 +5,11 @@
 -- Date: 
 -- Note: 
 -- =================================================================
+create table images (
+    image_id     number primary key
+    , file_name    varchar2(255)
+    , content_type varchar2(50)
+    , image_data   blob
+    , file_size    number
+    , created_at   timestamp
+);
